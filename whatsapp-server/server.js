b1b54c49820919f8b6c2fe0718d04c996db01d20
Manas Garg <manasgrg@gmail.com>
2026-09-11 23:15:06 +0530

    Remove single-process flag to prevent session lock conflicts

--- whatsapp-server/server.js
+++ whatsapp-server/server.js
@@ -257,7 +257,6 @@ const client = new Client({
       '--disable-accelerated-2d-canvas',
       '--no-first-run',
       '--no-zygote',
-      '--single-process',
       '--disable-gpu',
       '--disable-software-rasterizer',
       '--disable-extensions',
